62ccb4e62429b42a6a15e2c5526f18833c628c06
kodchamon  boonjan <117715922+DiaBmond@users.noreply.github.com>
2023-07-20 15:43:04 +0700

    add lab3

---
+++ lab3q4_65005.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    int sum=0,n=0,a;
+    float avg;
+    do{
+      printf("enter a number : ");
+      scanf("%d",&a);
+      if(a<0)break;
+      n++;
+      sum+=a;
+    }while(a>0);
+    if(a==0)n--;
+
+    //int a,b,c;
+    //printf("enter a number : ");
+    //scanf("%d",&a);
+    //printf("enter two number : ");
+    //scanf("%d %d",&b,&c);
+
+    avg = sum / n;
+    printf("summation = %d\n",sum);
+    printf("average = %f\n",avg);
+    return 0;
+}
